02930696c0e4b7ce6ce43f6eeb21de188f507574
mhayasi2009 <77230578+mhayasi2009@users.noreply.github.com>
2021-02-14 16:15:07 +0900

    Update m_h_music.js

--- m_h_music.js
+++ m_h_music.js
@@ -1,4 +1,5 @@
 $(function(){
+    // モーダル
     $('.header-login').click(function( ){
         $('#login-mordal-wrapper').fadeIn();
     });
@@ -7,21 +8,36 @@ $(function(){
         $('#login-mordal-wrapper').fadeOut();
     });
 
-    
+    // ヘッダーのNaviボタン
     $('.navi-btn').click(function(){
         var id = $(this).attr('data-navi');
         var position = $(id).offset().top;
         $('html, body').animate({'scrollTop':position},1500);
     })
 
-    $('.list-item').hover(
-        function(){
-            $(this).find('.message').fadeIn(1000);
-        },
-        function(){
-            $(this).find('.message').fadeOut(1000);
-    }); 
-    
+//list-item(ギャラリー)の開閉
+    /*list-item(ギャラリー)のhover動作
+     $('.list-item').hover(
+         function(){
+             $(this).find('.message').fadeIn(1000);
+         },
+         function(){
+             $(this).find('.message').fadeOut(1000);
+     }); */
+
+    //  list-item(ギャラリー)のクリック動作
+    $('.list-item').click(function() {
+        var $message = $(this).find('.message');
+        if($message.hasClass('message-open')) {
+            $message.removeClass('message-open');
+            $message.fadeOut();
+        } else {
+            $message.addClass('message-open');
+            $message.fadeIn();
+        }
+    })
+
+// F&Qの開閉　openクラスの有無で判断
     $('.faq-list-item').click(function() {
         var $faqanswer = $(this).find('.faq-answer');
         if($faqanswer.hasClass('open')) {
